21d48b607fae9fbce75b4088ddf06aa6d519f5b7
Natalie Clark <nj_clark@hotmail.co.uk>
2023-09-29 17:57:24 +0100

    Remove exception from Diary class

--- lib/diary.py
+++ lib/diary.py
@@ -1,5 +1,3 @@
-from lib.diary_entry import DiaryEntry
-
 class Diary():
     def __init__(self):
         self._entries = []
@@ -11,8 +9,6 @@ class Diary():
         #   nothing
         # Side-effect:
         #   appends instance to a list of diary entries
-        if not isinstance(diary_entry, DiaryEntry):
-            raise Exception("Diary entry must be an instance of DiaryEntry class")
         self._entries.append(diary_entry)
 
     def all(self):
